f2b5b864b3fad053efaa13f77f69d369874869d3
Ahmed <537705@student.saxion.nl>
2025-05-14 18:00:58 +0200

    finished connecting tiles

--- Assets/Scripts/Dungeon/DungeonBuilder.cs
+++ Assets/Scripts/Dungeon/DungeonBuilder.cs
@@ -1,9 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using Unity.AI.Navigation;
-using Unity.Collections;
-using Unity.Mathematics;
 using UnityEngine;
 
 public class DungeonBuilder : MonoBehaviour
@@ -26,6 +23,7 @@ public class DungeonBuilder : MonoBehaviour
     [SerializeField] private NavMeshSurface navMeshSurface;
 
     private int [,] _tileMap;
+    private Graph<Vector3> _traversalGraph = new();
 
     private const float SPAWN_OFFSET = 1f;
 
@@ -46,6 +44,14 @@ public class DungeonBuilder : MonoBehaviour
 
         painter.PaintGraph(graph, Color.green, Color.white);
         painter.PaintPath(graph, Color.blue, Color.blue);
+
+        foreach (var node in _traversalGraph.GetNodes()) {
+            DebugExtension.DebugWireSphere(node, Color.cyan, .2f);
+
+            foreach (var neighbor in _traversalGraph.GetNeighbors(node)) {
+                Debug.DrawLine(node, neighbor, Color.red);
+            }
+        }
     }
 
     public void StartSpawnWalls() {
@@ -53,53 +59,9 @@ public class DungeonBuilder : MonoBehaviour
     }
 
     public void StartSpawnFloor() {
-        Room startRoom = generator.Rooms[UnityEngine.Random.Range(0, generator.Rooms.Count - 1)];
+        Room startRoom = generator.Rooms[Random.Range(0, generator.Rooms.Count - 1)];
         StartCoroutine(SpawnFloor(new Vector2Int((int)startRoom.Bounds.center.x, (int)startRoom.Bounds.center.y)));
     }
-
-    // private IEnumerator SpawnWalls() {
-    //     HashSet<Vector3> doorPositions = generator.Doors.Select(d => new Vector3(d.position.x, 0, d.position.y)).ToHashSet();
-
-    //     Vector3 wallBottomOffset = new(SPAWN_OFFSET, 0, SPAWN_OFFSET);
-    //     Vector3 wallTopOffset = new(SPAWN_OFFSET, 0, -SPAWN_OFFSET);
-    //     Vector3 wallLeftOffset = new(SPAWN_OFFSET, 0, SPAWN_OFFSET);
-    //     Vector3 wallRightOffset = new(-SPAWN_OFFSET, 0, SPAWN_OFFSET);
-
-    //     foreach (Room room in generator.Rooms) {        
-    //         HashSet<Vector3> wallPositions = new();
-
-    //         for (int i = 0; i < room.Size.x; i++) {
-    //             Vector3 wallBottom = new(room.Position.x + i, 0, room.Position.y);
-    //             Vector3 wallTop = new(room.Position.x + i, 0, room.Position.y + room.Size.y);
-
-    //             wallPositions.Add(wallBottom + wallBottomOffset);
-    //             wallPositions.Add(wallTop + wallTopOffset);
-    //         }
-
-    //         for (int j = 0; j < room.Size.y; j++) {
-    //             Vector3 wallLeft = new(room.Position.x, 0, room.Position.y + j);
-    //             Vector3 wallRight = new(room.Position.x + room.Size.x, 0, room.Position.y + j);
-
-    //             wallPositions.Add(wallLeft + wallLeftOffset);
-    //             wallPositions.Add(wallRight + wallRightOffset);
-    //         }
-
-    //         GameObject wallParent = new("Room_" + room.Position + "_" + room.Size);
-    //         wallParent.transform.parent = roomParent.transform;
-
-    //         GameObject floor = Instantiate(floorPrefab, new Vector3(room.Bounds.center.x, 0, room.Bounds.center.y), floorPrefab.transform.rotation, wallParent.transform);
-    //         floor.transform.localScale = new Vector3(room.Size.x, room.Size.y, 1);
-
-    //         foreach (Vector3 wallPos in wallPositions) {
-    //             if (doorPositions.Contains(wallPos - wallBottomOffset) || doorPositions.Contains(wallPos - wallLeftOffset)) continue; 
-
-    //             // GameObject wall = Instantiate(wallPrefab, wallPos, Quaternion.identity, wallParent.transform);
-    //             // wall.name = "Wall_" + wall.transform.position;  
-
-    //             if (generationType != GenerationType.INSTANT) yield return GenerationHelper.WaitForGeneration(generationType, timeBetweenOperations);
-    //         }
-    //     }
-    // }
 
     private IEnumerator SpawnTiles() {
         foreach (Room room in generator.Rooms) {
@@ -121,48 +83,7 @@ public class DungeonBuilder : MonoBehaviour
         }   
     }
 
-    // private IEnumerator SpawnFloor(int startY, int startX) {        
-    //     int height = _tileMap.GetLength(0);
-    //     int width = _tileMap.GetLength(1);
-    //     bool[,] visited = new bool[height, width];
-    //     Queue<Vector2Int> tileQ = new();
-
-    //     tileQ.Enqueue(new Vector2Int(startX, startY));
-    //     visited[startY, startX] = true;
-
-    //     while (tileQ.Count > 0) {
-    //         Vector2Int currentTile = tileQ.Dequeue();
-    //         int x = currentTile.x;
-    //         int y = currentTile.y;
-
-    //         if (y < 0 || y >= height - 1 || x < 0 || x >= width - 1) continue;
-
-            // GameObject tileType = tilePrefabs[CalculateTileCase(x, y)];
-
-            // if (tileType.CompareTag("Filler")) Instantiate(floorPrefab, new Vector3(x + SPAWN_OFFSET, 0, y + SPAWN_OFFSET), floorPrefab.transform.rotation, floorParent.transform);
-
-    //         if (generationType != GenerationType.INSTANT) yield return GenerationHelper.WaitForGeneration(generationType, timeBetweenOperations);
-
-    //         Vector2Int[] dir = {
-    //             new(x, y + 1),
-    //             new(x, y - 1),
-    //             new(x + 1, y),
-    //             new(x - 1, y)
-    //         };
-
-    //         foreach (var next in dir) {
-    //             int nx = next.x;
-    //             int ny = next.y;
-
-    //             if (ny >= 0 && ny < height - 1 && nx >= 0 && nx < width - 1 && !visited[ny, nx]) {
-    //                 visited[ny, nx] = true;
-    //                 tileQ.Enqueue(next);
-    //             }
-    //         }
-    //     }
-    // }
-    
-    private IEnumerator SpawnFloor(Vector2Int startPoint) {        
+    private IEnumerator SpawnFloor(Vector2Int startPoint) {
         int oldTile = _tileMap[startPoint.y, startPoint.x];
 
         if (oldTile == 1) yield break;
@@ -171,27 +92,56 @@ public class DungeonBuilder : MonoBehaviour
         int width = _tileMap.GetLength(1);
 
         Queue<Vector2Int> tileQ = new();
-
         tileQ.Enqueue(startPoint);
-
         _tileMap[startPoint.y, startPoint.x] = 1;
 
-        Instantiate(floorPrefab, new Vector3(startPoint.x + SPAWN_OFFSET, 0, startPoint.y + SPAWN_OFFSET), floorPrefab.transform.rotation, floorParent.transform);
+        Vector3 spawnPos = new(startPoint.x, 0, startPoint.y);
+        Vector3 startNodePos = spawnPos + new Vector3(SPAWN_OFFSET / 2f, 0, SPAWN_OFFSET / 2f);
+        Instantiate(floorPrefab, spawnPos + new Vector3(SPAWN_OFFSET, 0, SPAWN_OFFSET), floorPrefab.transform.rotation, floorParent.transform);
+        _traversalGraph.AddNode(startNodePos);
 
         int[] dx = { -1, 1, 0, 0 };
-        int[] dy = { 0, 0, -1, 1};
+        int[] dy = { 0, 0, -1, 1 };
 
-        while (tileQ.Count > 0) {
+        while (tileQ.Count > 0)
+        {
             Vector2Int tile = tileQ.Dequeue();
 
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < 4; i++)
+            {
                 int nx = tile.x + dx[i];
                 int ny = tile.y + dy[i];
 
-                if (nx >= 0 && nx < height && ny >= 0 && ny < width && _tileMap[nx, ny] == 0) {
+                if (nx >= 0 && nx < height && ny >= 0 && ny < width && _tileMap[nx, ny] == 0)
+                {
                     _tileMap[nx, ny] = 1;
 
-                    Instantiate(floorPrefab, new Vector3(ny + SPAWN_OFFSET, 0, nx + SPAWN_OFFSET), floorPrefab.transform.rotation, floorParent.transform);
+                    spawnPos = new Vector3(ny, 0, nx);
+                    Vector3 nodePos = spawnPos + new Vector3(SPAWN_OFFSET / 2f, 0, SPAWN_OFFSET / 2f);
+
+                    Instantiate(floorPrefab, spawnPos + new Vector3(SPAWN_OFFSET, 0, SPAWN_OFFSET), floorPrefab.transform.rotation, floorParent.transform);
+                    _traversalGraph.AddNode(nodePos);
+
+                    List<Vector3> currentNodes = _traversalGraph.GetNodes();
+
+                    foreach (var dir in new Vector2Int[] {
+                        new(-1, 0),
+                        new(1, 0),
+                        new(0, -1),
+                        new(0, 1),
+
+                        new(-1, 1),
+                        new(1, 1),
+                        new(-1, -1),
+                        new(1, -1)
+                    }) {
+                        int neighborX = nx + dir.x;
+                        int neighborY = ny + dir.y;
+
+                        Vector3 neighborPos = new Vector3(neighborY, 0, neighborX) + new Vector3(SPAWN_OFFSET / 2f, 0, SPAWN_OFFSET / 2f);
+
+                        if (currentNodes.Contains(neighborPos)) _traversalGraph.AddEdge(nodePos, neighborPos);
+                    }
 
                     if (generationType != GenerationType.INSTANT) yield return GenerationHelper.WaitForGeneration(generationType, timeBetweenOperations);
 
